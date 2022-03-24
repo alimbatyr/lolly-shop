226d68dd1c65abc1d7f77ae2c1c2f8f2e2c3ad71
alepsibaeva <a.lepsibaeva@gmail.com>
2022-03-24 21:10:05 +0600

    psql init query

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -2,9 +2,27 @@ CREATE DATABASE shop;
 \connect shop;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  user_id PRIMARY KEY NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE products (
+  product_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price NUMERIC(10,2) NOT NULL,
+  images TEXT[], -- array of image ids
+  colors TEXT[], -- colors is an array of hex or rgba values --
+  sizes TEXT[], -- sizes is an array of size values --
+  user_id INTEGER NOT NULL REFERENCES users(user_id),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE images (
+  image_id SERIAL PRIMARY KEY,
+  filename VARCHAR(255) NOT NULL,
+  filepath VARCHAR(255) NOT NULL,
+  mimetype VARCHAR(255) NOT NULL,
+  size INTEGER NOT NULL
 );
